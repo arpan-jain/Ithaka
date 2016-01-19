9c24198ed900dd4953958a05283ee4c14b069364
arpan_jain <arpanjain3.14@gmail.com>
2016-01-19 12:06:50 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,24 +1,18 @@
 //@uthor:Arpan
 
-var exec=require("child_process").exec;
+function route(handle,pathname,response)
+{ 
+console.log("About to route a request for " + pathname); 
 
-function start(response){
- console.log("Request handler 'start' was called.");
- exec("ls -lah", function (error, stdout, stderr){ 
-     response.writeHead(200, {"Content-Type": "text/plain"}); 
-     response.write(stdout);
-     response.end(); 
-      }); 
- }
-
-
-function upload(response){
- console.log("Request handler 'upload' was called."); 
- response.writeHead(200, {"Content-Type": "text/plain"}); 
- response.write("Hello Upload");
- response.end();
- }
-
-
-exports.start=start; 
-exports.upload=upload;
+if (typeof handle[pathname] === 'function') {
+    handle[pathname](response); 
+    }
+else{
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+    response.end();
+  } }
+  
+  
+exports.route=route;
