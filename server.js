d358acca15db4d0dc422f4f4aff513ea26e71fd6
arpan_jain <arpanjain3.14@gmail.com>
2016-01-19 16:00:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function start(route,handle){
 function onRequest(request, response) {
  var pathname = url.parse(request.url).pathname; 
  console.log("Request for " + pathname + " received.");
- route(handle, pathname, response);
+ route(handle, pathname, request,response);
   }
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
