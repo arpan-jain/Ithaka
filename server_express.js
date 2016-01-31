cf320bdbb5d96ac65438c4776c861b50d6447c3b
arpan_jain <arpanjain3.14@gmail.com>
2016-01-31 19:48:34 +0530

    Update server_express.js

--- server_express.js
+++ server_express.js
@@ -1,10 +1,19 @@
 //@uthor:Arpan
-//using express for node.js
 var express = require('express');
 var app = express();
 var path = require('path');
-
-app.use(express.bodyParser());
+var bodyparser =require('body-parser');
+var mysql= require('mysql');
+var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : '6277',
+   database : 'ithaka'
+ });
+var md5 = require ('md5');
+ 
+ 
+app.use(bodyparser());
 
 var server = app.listen(8888);
 
@@ -21,8 +30,18 @@ app.get('/register.html', function(req, res) {
 });
  
 app.post('/register',function(req,res){
-  console.log(req.baseUrl);
-  console.log(req.params);
-  console.log(req.body);
+  var fname=req.body.fname;console.log(fname);
+  var lname=req.body.lname;console.log(lname);
+  var gender=req.body.gender;console.log(gender);
+  var email_id=req.body.email;console.log(email_id);
+  var pass=md5(req.body.pass);console.log(pass);
+  connection.connect();
+  var query= connection.query('insert into user(fname,lname,gender,email_id,password) values(\"'+fname+'\",\"'+lname+'\",\"'+gender+'\",\"'+email_id+'\",\"'+pass+'\")', function(err, rows, fields) {
+   if (err)
+     console.log('Error while performing Query.');
+ });
+  console.log(query.sql);
   res.status(200).send('Hey You have been registered in our database');
+  
+ connection.end();
 }); 
